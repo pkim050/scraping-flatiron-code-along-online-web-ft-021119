afe9f44a0a122d9d05098e3798a0a0b97d8cd951
Patrick Kim <pkim050@gmail.com>
2019-03-07 22:34:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-    html = open()
+    html = open(https://learn-co-curriculum.github.io/site-for-scraping/courses)
   end
   
   def get_courses
